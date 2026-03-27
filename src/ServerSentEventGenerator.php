30015f982eed2a026b08eb0efec203c997a0430a
Ben Croker <57572400+bencroker@users.noreply.github.com>
2026-03-27 11:31:53 -0600

    Handle DELETE method in `ReadSignals`

--- src/ServerSentEventGenerator.php
+++ src/ServerSentEventGenerator.php
@@ -44,7 +44,11 @@ class ServerSentEventGenerator
      */
     public static function readSignals(): array
     {
-        $input = $_GET[Consts::DATASTAR_KEY] ?? file_get_contents('php://input');
+        if ($_SERVER['REQUEST_METHOD'] === 'GET' || $_SERVER['REQUEST_METHOD'] === 'DELETE') {
+            $input = $_GET[Consts::DATASTAR_KEY];
+        } else {
+            $input = file_get_contents('php://input');
+        }
         $signals = $input ? json_decode($input, true) : [];
 
         return is_array($signals) ? $signals : [];
